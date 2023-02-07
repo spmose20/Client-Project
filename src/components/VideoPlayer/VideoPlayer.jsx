b8851582ccf34949cc046447ad92e33a84192580
Micah G <113459878+MicahGolden@users.noreply.github.com>
2023-02-07 14:45:24 -0600

    Added VideoPlayer

---
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './VideoPlayer.css'
+const Player = () => {
+    return (
+        <div>
+     <iframe width="660" height="415" src="https://www.youtube.com/embed/sVbnwpJLCCE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+    )
+}
+
+export default Player;
