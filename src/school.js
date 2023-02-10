c7defd2e300460c53d5d956e6378f249c921e067
Micah G <113459878+MicahGolden@users.noreply.github.com>
2023-02-10 14:33:42 -0600

    Add files via upload

---
+++ src/school.js
@@ -0,0 +1,36 @@
+const School = [
+{ 
+  id: 1, 
+  person: "Sam",
+  title: "Student",
+},
+{ 
+  id: 2,
+  person: "Micah",
+  title: "Student",
+  
+},
+{ 
+  id: 3,
+  person: "Roma",
+  title: "Student",
+  
+},
+{ 
+  id: 4, 
+  person: "Ash",
+  title: "Teacher",
+  
+},
+{ 
+  id: 5,
+  person: "Steph",
+  title: "Teacher",
+},
+{ 
+  id: 6,
+  person: "Ben",
+  title: "Teacher",
+},
+
+];
